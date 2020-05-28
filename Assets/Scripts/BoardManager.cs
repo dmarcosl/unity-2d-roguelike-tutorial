5ba90f2b1f57ab428557c5ac5fff3192155ab708
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2020-05-29 00:18:47 +0200

    Added board, game and loader scripts

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int columns = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        // Empty the grid
+        gridPositions.Clear();
+
+        // Add positions of the inner grid, leaving a inner border between the grid and the outer walls
+        for (var x = 1; x < columns - 1; x++)
+        {
+            for (var y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        // Iterate the grid including the outer walls
+        for (var x = -1; x < columns + 1; x++)
+        {
+            for (var y = -1; y < rows + 1; y++)
+            {
+                // Instantiate the floor and outer walls
+                GameObject toInstantiate;
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                }
+                else
+                {
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                }
+
+                var instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity);
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Select a random available position of the grid and returns it
+    /// </summary>
+    /// <returns>Any random available position of the grid</returns>
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    /// <summary>
+    /// Instantiate a random number of random gameObjects in random positions
+    /// </summary>
+    /// <param name="tileArray">Array of gameObjects to instantiate</param>
+    /// <param name="minimum">Min number of gameObjects to instantiate</param>
+    /// <param name="maximum">Max number of gameObjects to instantiate</param>
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitialiseList();
+        LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.minimum);
+        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.minimum);
+
+        int enemyCount = (int) Mathf.Log(level, 2f);
+        LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+    }
+}
